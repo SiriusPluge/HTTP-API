08c511da56beaba710e3389b3f8615c8df836917
SiriusPluge <SiriusPluge@github.com>
2021-10-18 10:37:22 +0300

    0.8

--- internal/postresDB.go
+++ internal/postresDB.go
@@ -4,23 +4,18 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/joho/godotenv"
-	"os"
 )
 
 func OpenConnection() *sql.DB{
-	e := godotenv.Load() //Download .env
-	if e != nil {
-		fmt.Print(e)
-	}
 
-	username := os.Getenv("db_user")
-	password := os.Getenv("db_pass")
-	dbName := os.Getenv("db_name")
-	dbHost := os.Getenv("db_host")
+	username := "postgres"
+	password := "qwerty"
+	dbName := "postgres"
+	dbHost := "localhost"
+	dbPort := "5432"
 
 
-	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Создать строку подключения
+	dbUri := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbPort, username, dbName, password) //Создать строку подключения
 	fmt.Println(dbUri)
 
 	DB, err := sql.Open("postgres", dbUri)
